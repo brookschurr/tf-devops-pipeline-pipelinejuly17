96b73223bc852e91e87640178046913c28ac215f
brookschurr <83605897+brookschurr@users.noreply.github.com>
2025-07-17 22:11:00 +0000

    Update main1.tf

--- main1.tf
+++ main1.tf
@@ -1,9 +1,1 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
-}
 
-resource "azurerm_resource_group" "rg" {
-name     = "bs-basic-pipeline"
-location = "westus2"
-}
