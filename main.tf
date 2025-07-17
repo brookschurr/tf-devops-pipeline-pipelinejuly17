a19e696e3b82ebd7cdbd2e25ed64577829d52b4e
brookschurr <83605897+brookschurr@users.noreply.github.com>
2025-07-17 22:14:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
 name     = "bs-basic-pipeline"
 location = "westus2"
 }
