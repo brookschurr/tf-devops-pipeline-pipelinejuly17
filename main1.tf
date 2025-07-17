c79fe0a8250921b9ece3dd6d9f187cc91f51076c
brookschurr <83605897+brookschurr@users.noreply.github.com>
2025-07-17 22:13:18 +0000

    Update main1.tf

--- main1.tf
+++ main1.tf
@@ -1,1 +1,18 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
+}
 
+resource "azurerm_resource_group" "rg" {
+name     = "bs-basic-pipeline"
+location = "westus2"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "bs-state-rg-pipeline"
+    storage_account_name = "bsstatesapipline"
+    container_name       = "bs-tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
